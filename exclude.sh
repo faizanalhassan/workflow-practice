9150fc3297e7b4da7ebfe2b7161c3a9a04254f8e
Faizan Alhassan <faizanalhassan@gmail.com>
2022-05-13 01:31:18 +0500

    test 7 (2)

--- exclude.sh
+++ exclude.sh
@@ -2,9 +2,9 @@ if [ -f exclude-items.txt ]; then
     echo Found exclude-items.txt. Using it to remove files
     sed $'s/\r$//' ./exclude-items.txt > ./exclude-items.unix.txt
     cat exclude-items.unix.txt | xargs -d '\n' rm -rf
+    rm exclude-items.unix.txt exclude-items.txt
     echo Files after removing exclude list
     ls
-    rm exclude-items.unix.txt exclude-items.txt
 else
     echo exclude file does not found.
 fi
